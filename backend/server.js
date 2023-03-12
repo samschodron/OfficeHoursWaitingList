08e74ccd93ca8d06401d2d688345939f41363703
Charmaine Seah <charmaineseahsy@gmail.com>
2023-03-12 11:48:50 -0500

    added createWaitingRoom route and controller, and fixed server.js to use bodyParser

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,20 @@
+import waitingRoomRoutes from './routes/waitingRoom.js'
+import bodyParser from "body-parser";
+
+// Define "require"
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+
 const express = require('express')
+const app = express()
 const port = process.env.PORT || 31415
 
-const app = express()
+// for parsing application/ json
+app.use(bodyParser.json())
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use('/waitingRoom', waitingRoomRoutes)
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}`)
