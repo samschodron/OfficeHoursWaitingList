105088a561e743d7ddc79aa5cedb7a946d60e7dc
schodron@wisc.edu <schodron@wisc.edu>
2023-03-24 20:57:57 -0500

    writing APIs

--- backend/studentJoin.js
+++ backend/studentJoin.js
@@ -1,15 +1,18 @@
+import db from './dbconfig.js'
+
 /* PARAMS: The room code of list student is trying to join and student name.
    This function creates a SQL insert statement that adds the student to 
-   the 
+   the wait list.
     */
-function studentJoin(code, name) {
+function studentJoin(code, firstName, secondName) {
     /* Going to need to create some sort of random number generator
        and check if the ID is in the table. */
 
     const id = 1; // TODO: decide how we want to generate unique ID
     const time = Date.now();
-    const sqlInsert = "INSERT INTO @@room_visitors (visitor_id, room_code, visitor_name, time_entered, time_left)";
-    const values = (`(${id}, ${code}, ${name}, ${time}, NULL);`);
 
-    // Need to find out how to connect to database to finish
+    db.query(`INSERT INTO student (studentID_pk, student_first_name, student_last_name, time_entered, time_left, room_ID, is_waiting) VALUES (${id}, ${firstName}, ${secondName}, ${time}, NULL, ${code}, 1)`, function (err, result, fields) {
+        if (err) throw err;
+        console.log('Successfully added to the wait list.');
+    })
 }
