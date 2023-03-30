a9d4569aea2c6394cb6a516b0966c8b50bde9731
Charmaine Seah <charmaineseahsy@gmail.com>
2023-03-29 20:04:39 -0500

    get all students in waiting room api complete and added waiting room name field for create waiting room api

--- backend/controllers/validators/waitingRoomValidator.js
+++ backend/controllers/validators/waitingRoomValidator.js
@@ -3,7 +3,8 @@ import * as yup from 'yup';
 export const createWaitingRoomSchema = yup
     .object({
         teaching_assistant_first_name: yup.string().trim().required(),
-        teaching_assistant_last_name: yup.string().trim().required()
+        teaching_assistant_last_name: yup.string().trim().required(),
+        waiting_room_name: yup.string().trim().required()
     })
     .required();
 
--- backend/controllers/waitingRoom.js
+++ backend/controllers/waitingRoom.js
@@ -16,23 +16,25 @@ export const createWaitingRoom = async (req, res) => {
         const data = createWaitingRoomSchema.validateSync(body, { abortEarly: false, stripUnknown: true });
         let teachingAssistantFirstName = data['teaching_assistant_first_name']
         let teachingAssistantlastName = data['teaching_assistant_last_name']
+        let waitingRoomName = data['waiting_room_name']
 
         let roomCode = generateUniqueRoomCode()
 
-        let sqlQuery = `INSERT INTO teaching_assistant (room_code_pk, teaching_assistant_first_name, teaching_assistant_last_name, time_created) VALUES ('${roomCode}', '${teachingAssistantFirstName}', '${teachingAssistantlastName}', now())`;
+        let sqlQuery = `INSERT INTO teaching_assistant (room_code_pk, teaching_assistant_first_name, teaching_assistant_last_name, time_created, waiting_room_name) VALUES ("${roomCode}", "${teachingAssistantFirstName}", "${teachingAssistantlastName}", now(), "${waitingRoomName}")`;
 
         db.query(sqlQuery, function (error, result, fields) {
             if (error) {
                 res.status(400).json({ message: 'failed to create a waiting room' })
                 throw error;
+            } else {
+                return res.json({
+                    message: 'successfully created waiting room',
+                    data,
+                    room_code: roomCode,
+                    waiting_room_name: waitingRoomName
+                });
             }
         })
-
-        return res.json({
-            message: 'successfully created waiting room',
-            data,
-            room_code: roomCode
-        });
     } catch (error) {
         return res.status(422).json({ errors: error.errors });
     }
@@ -42,35 +44,22 @@ export const getAllStudentsInWaitingRoom = async (req, res) => {
     const { body } = req
     try {
         const data = getAllStudentsInWaitingRoomSchema.validateSync(body, { abortEarly: false, stripUnknown: true })
-        console.log(data)
         const roomCode = data['room_code_pk']
-        console.log(roomCode)
 
-        let sqlQuery = `SELECT student_first_name, student_last_name FROM student WHERE is_waiting = 1 AND room_ID = '${roomCode}' ORDER BY time_entered DESC;`
-
-        let queryResult;
+        let sqlQuery = `SELECT student_first_name, student_last_name, time_entered FROM student WHERE is_waiting = 1 AND room_code_pk = "${roomCode}" ORDER BY time_entered;`
 
         db.query(sqlQuery, function (error, result, fields) {
-            queryResult = result;
-            console.log(result)
             if (error) {
-                console.log('here 1')
                 res.status(400).json({ message: 'failed to retrieve list of students in the waiting room' })
                 throw error;
+            } else {
+                return res.json({
+                    message: 'successfully retrieved list of students in the waiting room',
+                    query_result: result
+                })
             }
         });
-        // console.log(result)
-
-        if (queryResult) {
-            console.log(true)
-        } else {
-            console.log(false)
-        }
-        return res.json({
-            message: 'successfully retrieved list of students in the waiting room'
-        })
     } catch (error) {
-        console.log('here 2')
         return res.status(422).json({ errors: error.errors });
     }
 }
