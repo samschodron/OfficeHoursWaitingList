c8e6739e76d2cf1f260c8b1bcdeaac611a356daa
schodron@wisc.edu <schodron@wisc.edu>
2023-04-03 12:04:50 -0500

    Changing routes

--- backend/routes/student.js
+++ backend/routes/student.js
@@ -1,7 +1,13 @@
 import express from 'express'
+import { joinWaitingRoom } from '../controllers/student.js';
 import { leaveWaitingRoom } from '../controllers/student.js';
 
 const router = express.Router();
+
+/*
+    path from root: /student/joinWaitingRoom
+*/
+router.post('/joinWaitingRoom', joinWaitingRoom)
 
 /*
     path from root: /student/leaveWaitingRoom
