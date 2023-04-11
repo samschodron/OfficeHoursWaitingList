677b8fe0946bedc1dccd3ae0d16c7a3f46a49543
carlino <ccarlino@wisc.edu>
2023-04-11 17:36:05 -0500

    destroy waiting room without docs

--- backend/routes/waitingRoom.js
+++ backend/routes/waitingRoom.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import { createWaitingRoom } from '../controllers/waitingRoom.js';
 import { getAllStudentsInWaitingRoom } from '../controllers/waitingRoom.js';
-
+import { destroyWaitingRoom } from '../controllers/waitingRoom.js';
 const router = express.Router();
 
 /*
@@ -13,6 +13,11 @@ router.post('/createWaitingRoom', createWaitingRoom)
     path from root: /waitingRoom/getAllStudentsInWaitingRoom
 */
 router.get('/getAllStudentsInWaitingRoom', getAllStudentsInWaitingRoom)
+
+/*
+    path from root: /waitingRoom/destroyWaitingRoom
+*/
+router.post('/destroyWaitingRoom', destroyWaitingRoom)
 
 
 export default router;
