edf33d081f8fbd5e6c9060e2f5c6cf365aed971c
Henry Li <hli779@wisc.edu>
2023-04-25 17:06:50 -0500

    modified styling of dashbaord to be more consistent with app styling

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -3,11 +3,43 @@ import { onAuthStateChanged } from "firebase/auth";
 import { signOut } from "firebase/auth";
 import { auth } from '../firebase';
 import { useNavigate } from 'react-router-dom';
-import { Container, Typography, Box, Button, Grid } from '@mui/material';
+import { Container, Typography, Box, Button, Grid, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
 import { Link } from 'react-router-dom';
+import {makeStyles} from "@mui/styles";
+
+const useStyles = makeStyles((theme) => ({
+    formContainer: {
+        display: 'flex',
+        flexDirection: 'column',
+        gap: theme.spacing(2),
+    },
+    title: {
+        marginTop: theme.spacing(4),
+        marginBottom: theme.spacing(2),
+    },
+    signUpLink: {
+        marginTop: theme.spacing(2),
+        textAlign: 'center',
+    },
+    root: {
+        minHeight: '100vh',
+        backgroundImage: 'linear-gradient(45deg, #C5AAE5FF 30%, #7e50f2 90%)',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    paper: {
+        padding: theme.spacing(4),
+        backgroundColor: 'white',
+        borderRadius: theme.spacing(1),
+    },
+    error: {
+        color: 'red'
+    }
+}));
 
 const Dashboard = () => {
-
+    const classes = useStyles();
     const navigate = useNavigate();
     const [openWaitingLists, setOpenWaitingLists] = useState([])
     const [joinedWaitingLists, setJoinedWaitingLists] = useState([])
@@ -46,6 +78,7 @@ const Dashboard = () => {
             .then(res => res.json())
             .then(data => {
                 let joinedWaitingLists = data["query_result"]
+                console.log(joinedWaitingLists)
                 setJoinedWaitingLists(joinedWaitingLists)
             })
     }
@@ -112,35 +145,35 @@ const Dashboard = () => {
     }
 
     return (
-        <Container maxWidth="lg">
-            <Box mt={4}>
-                <Typography variant="h3" sx={{ fontWeight: "bold" }} gutterBottom>
-                    Dashboard
-                </Typography>
+        <div className={classes.root}>
+            <Container maxWidth="lg">
+                <Box mt={4} p={4} bgcolor="white" borderRadius={2} boxShadow={3}>
+                    <Typography variant="h3" sx={{ fontWeight: "bold" }} gutterBottom>
+                        Dashboard
+                        <Typography variant="h5" sx={{marginTop: -5, textAlign: "right"}} gutterBottom>
+                            Welcome,
+                        </Typography>
 
-                <Typography variant="h5" gutterBottom>
-                    You are currently signed in as:
-                </Typography>
-
-                {auth.currentUser ? (
-                    <Typography variant="h6" gutterBottom>
-                        <strong>Email:</strong> {auth.currentUser.email}
+                        {auth.currentUser ? (
+                            <Typography variant="h6" sx={{marginTop: -1, textAlign: "right"}} gutterBottom>
+                                <strong> {auth.currentUser.email} </strong>
+                            </Typography>
+                        ) : (
+                            <Typography variant="h6" gutterBottom>
+                                Not signed in
+                            </Typography>
+                        )}
                     </Typography>
-                ) : (
-                    <Typography variant="h6" gutterBottom>
-                        Not signed in
-                    </Typography>
-                )}
-
                 <Grid container spacing={2} mt={2}>
                     <Grid item xs={12} sm={6}>
                         <Button
                             component={Link}
                             to="/join-page"
-                            variant="contained"
-                            color="primary"
+                            variant="outlined"
                             fullWidth
-                            sx={{ fontSize: "30px", fontWeight: "bold", borderRadius: 1, paddingY: 20 }}
+                            sx={{color: "black", borderWidth: "3px", borderColor: "black", fontSize: "30px", fontWeight: "bold", borderRadius: 1, paddingY: 20,  '&:hover': {
+                                    background: "rgba(0, 0, 0, 0.1)", borderWidth: "3px", borderColor: "black"
+                                }}}
                         >
                             Join A List
                         </Button>
@@ -150,10 +183,11 @@ const Dashboard = () => {
                         <Button
                             component={Link}
                             to="/create-list-page"
-                            variant="contained"
-                            color="primary"
+                            variant="outlined"
                             fullWidth
-                            sx={{ fontSize: "30px", fontWeight: "bold", borderRadius: 1, paddingY: 20 }}
+                            sx={{color: "black", borderWidth: "3px", borderColor: "black", fontSize: "30px", fontWeight: "bold", borderRadius: 1, paddingY: 20,  '&:hover': {
+                                    background: "rgba(0, 0, 0, 0.1)", borderWidth: "3px", borderColor: "black"
+                                }}}
                         >
                             Create A List
                         </Button>
@@ -163,69 +197,91 @@ const Dashboard = () => {
                 <Box mt={2}>
                     <Button
                         variant="contained"
-                        color="secondary"
                         fullWidth
                         onClick={handleLogout}
-                        sx={{ fontWeight: "bold", borderRadius: 1, paddingY: 2 }}
+                        sx={{background: "#000000", fontWeight: "bold", borderRadius: 1, paddingY: 2, '&:hover': { background: '#000000', opacity: 0.7, transition: '.2s'} }}
                     >
                         Logout
                     </Button>
                 </Box>
             </Box>
-            <Typography variant="h3">All open waiting lists:</Typography>
-            <Box sx={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap' }}>
-                <Typography variant="h5" gutterBottom >
-                    {openWaitingLists.map(waitingList => {
-                        let firstName = waitingList["teaching_assistant_first_name"]
-                        let lastName = waitingList["teaching_assistant_last_name"]
-                        let roomName = waitingList["waiting_room_name"]
-                        let roomCode = waitingList["room_code_pk"]
+                <TableContainer component={Paper} sx={{borderRadius: 2, boxShadow: 3, paddingTop: 2, marginTop: 3}}>
+                    <Typography variant="h4" sx={{textAlign: "center", fontWeight: "Bold", marginBottom: 3}}>Open Lists</Typography>
+                    <Table sx={{ minWidth: 650 }} aria-label="open waiting lists">
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>TA Name</TableCell>
+                                <TableCell>Room Name</TableCell>
+                                <TableCell>Room Code</TableCell>
+                                <TableCell>Action</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {openWaitingLists.map(waitingList => {
+                                let firstName = waitingList["teaching_assistant_first_name"]
+                                let lastName = waitingList["teaching_assistant_last_name"]
+                                let roomName = waitingList["waiting_room_name"]
+                                let roomCode = waitingList["room_code_pk"]
 
-                        return (
-                            <Box sx={{ border: '3px solid black', margin: '5px', borderRadius: '10px', minWidth: '600px' }}>
-                                <h3>TA name: {firstName} {lastName}</h3>
-                                <h3>room name: {roomName}</h3>
-                                <h3>room code: {roomCode}</h3>
-                                <Box onClick={() => navigateToWaitingListPage(firstName, lastName, roomName, roomCode)}
-                                >
-                                    <Button variant="contained" className="shadow" sx={{
-                                        color: 'white', borderRadius: '30px', minWidth: '35%',
-                                        minHeight: '3rem', background: 'primary', '&:hover': { background: '#000000', opacity: 0.7, transition: '.2s' }
-                                    }}>
-                                        Enter this waitlist
-                                    </Button>
-                                </Box>
-                            </Box>)
-                    })}
-                </Typography>
-            </Box>
-            <Typography variant="h3">All joined waiting lists:</Typography>
-            <Box sx={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap' }}>
-                <Typography variant="h5" gutterBottom >
-                    {joinedWaitingLists.map(joinedList => {
-                        let firstName = joinedList["student_first_name"]
-                        let lastName = joinedList["student_last_name"]
-                        let roomCode = joinedList["room_code_pk"]
-                        let studentID = joinedList["studentID_pk"]
+                                return (
+                                    <TableRow key={roomCode}>
+                                        <TableCell>{firstName} {lastName}</TableCell>
+                                        <TableCell>{roomName}</TableCell>
+                                        <TableCell>{roomCode}</TableCell>
+                                        <TableCell>
+                                            <Button variant="contained" className="shadow" sx={{
+                                                color: 'white', borderRadius: '30px', minWidth: '35%',
+                                                minHeight: '3rem', background: '#000000', '&:hover': { background: '#000000', opacity: 0.7, transition: '.2s' }
+                                            }} onClick={() => navigateToWaitingListPage(firstName, lastName, roomName, roomCode)}>
+                                                Enter
+                                            </Button>
+                                        </TableCell>
+                                    </TableRow>
+                                );
+                            })}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+                <TableContainer component={Paper} sx={{borderRadius: 2, boxShadow: 3, paddingTop: 2, marginTop: 3}}>
+                    <Typography variant="h4" sx={{textAlign: "center", fontWeight: "Bold", marginBottom: 3}}>Joined Lists</Typography>
+                    <Table sx={{ minWidth: 650 }} aria-label="joined waiting lists">
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>Your Name</TableCell>
+                                {/*<TableCell>Room Name</TableCell>*/}
+                                <TableCell>Room Code</TableCell>
+                                <TableCell>Action</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {joinedWaitingLists.map(joinedList => {
+                                let firstName = joinedList["student_first_name"]
+                                let lastName = joinedList["student_last_name"]
+                                let roomCode = joinedList["room_code_pk"]
+                                let roomName = joinedList["waiting_room_name"]
+                                let studentID = joinedList["studentID_pk"]
 
-                        return (
-                            <Box sx={{ border: '3px solid black', margin: '5px', borderRadius: '10px', minWidth: '600px' }}>
-                                <h3>Your name: {firstName} {lastName}</h3>
-                                <h3>room code: {roomCode}</h3>
-                                <Box onClick={() => navigateToJoinedListPage(firstName, lastName, studentID, roomCode)}
-                                >
-                                    <Button variant="contained" className="shadow" sx={{
-                                        color: 'white', borderRadius: '30px', minWidth: '35%',
-                                        minHeight: '3rem', background: 'primary', '&:hover': { background: '#000000', opacity: 0.7, transition: '.2s' }
-                                    }}>
-                                        Enter this waitlist
-                                    </Button>
-                                </Box>
-                            </Box>)
-                    })}
-                </Typography>
-            </Box>
-        </Container >
+                                return (
+                                    <TableRow key={studentID}>
+                                        <TableCell>{firstName} {lastName}</TableCell>
+                                        {/*<TableCell>{roomName}</TableCell>*/}
+                                        <TableCell>{roomCode}</TableCell>
+                                        <TableCell>
+                                            <Button variant="contained" className="shadow" sx={{
+                                                color: 'white', borderRadius: '30px', minWidth: '35%',
+                                                minHeight: '3rem', background: '#000000', '&:hover': { background: '#000000', opacity: 0.7, transition: '.2s' }
+                                            }} onClick={() => navigateToJoinedListPage(firstName, lastName, studentID, roomCode)}>
+                                                Enter
+                                            </Button>
+                                        </TableCell>
+                                    </TableRow>
+                                );
+                            })}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+        </Container>
+        </div>
     );
 };
 
